524ffd68ca02a3715f6faeddeec3c615a29bac9b
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2025-08-05 15:02:40 -0300

    Ajuste link redes sociais

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
   <div class="footer-content">
     <div class="footer-column">
       <h3>Revvo</h3>
-      <p>Educação que transforma.</p>
+      <p>Soluções em Aprendizagem Digital</p>
     </div>
     <div class="footer-column">
       <h4>Contato</h4>
@@ -12,9 +12,8 @@
     <div class="footer-column">
       <h4>Siga-nos</h4>
       <div class="social-icons">
-        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" /></a>
-        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733558.png" alt="Instagram" /></a>
-        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="LinkedIn" /></a>
+        <a href="https://www.instagram.com/somosrevvo/  "><img src="https://cdn-icons-png.flaticon.com/512/733/733558.png" alt="Instagram" /></a>
+        <a href="https://www.linkedin.com/company/somosrevvo/"><img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="LinkedIn" /></a>
       </div>
     </div>
   </div>
